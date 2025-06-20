29d5e270787348bd382628674285e872d9144e2e
Ayush Barnwal <ayushkr.barnwal.cse24@itbhu.ac.in>
2025-06-20 19:46:03 +0530

    fixed some login problem

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,6 @@ path('create/', views.create_post, name='create_post'),
     path('api/posts/update/<int:pk>/', views.api_update_post, name='api_update_post'),
     path('api/posts/delete/<int:pk>/', views.api_delete_post, name='api_delete_post'),
       path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
